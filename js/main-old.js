28fba9e866799350aed1934119da42e28ccf7f3c
Simone Sala <simonesala@me.com>
2020-04-09 13:06:40 +0200

    Codice corretto

---
+++ js/main-old.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "http://157.230.17.132:4032/sales",
+        method: "GET",
+        success: function(data) {
+            calcValues(data);
+
+        },
+        error: function(err) {
+            alert("Errore AJAX");
+        }
+
+    });
+
+    $("#aggiungi-fatturato").click(function(){
+        var venditore = $(".box-aggiunta-dati .select-venditore").val();
+        var fatturato = parseInt($(".box-aggiunta-dati .input-fatt").val());
+        var periodo = moment($(".box-aggiunta-dati .input-data").val()).format("DD/MM/YYYY");
+
+
+        $.ajax({
+            url: "http://157.230.17.132:4032/sales",
+            method: "POST",
+            data:{
+                salesman: venditore,
+                amount: fatturato,
+                date: periodo
+            },
+            success: function(data) {
+            },
+            error: function(err){
+                alert("Errore aggiornamento dati")
+            }
+        });
+
+        $.ajax({
+            url: "http://157.230.17.132:4032/sales",
+            method: "GET",
+            success: function(data) {
+                calcValues(data);
+
+            },
+            error: function(err) {
+                alert("Errore AJAX");
+            }
+
+        });
+
+    });
+
+
+
+    // --------------- FUNCTIONS ---------------------
+
+    function sortNumber(a, b) { // ordine discendente
+        return b - a;
+    };
+
+    function calcValues(data){
+        var datiOrganizzati = {}; // Torta
+        var datiMensili = {};     // Linea
+        for (var i = 0; i < data.length; i++) {
+            var dato = data[i];
+            var venditore = dato.salesman;
+            var id = dato.id;
+            var calendarioFornito = dato.date;
+            var quantitativo = parseInt(dato.amount);
+            var calendario = moment(calendarioFornito, "DD-MM-YYY");
+            var mese = calendario.format("M");
+            var arrayMesi = moment.months("MMMM");
+
+
+            if (datiOrganizzati[venditore] === undefined) { //Torta
+                datiOrganizzati[venditore] = 0;
+            }
+            datiOrganizzati[venditore] += quantitativo;
+
+            if (datiMensili[mese] === undefined) { //Linea
+                datiMensili[mese] = 0;
+            }
+            datiMensili[mese] += quantitativo;
+
+
+        }
+
+        var labels = [];// Torta
+        var values = [];
+        for (var key in datiOrganizzati) {
+            labels.push(key);
+            values.push(datiOrganizzati[key]);
+        }
+        values.sort(sortNumber);
+
+
+        var labelsMesi = [];//Linea
+        var valuesMesi = [];
+        for (var key in datiMensili) {
+            labelsMesi.push(key);
+            valuesMesi.push(datiMensili[key]);
+        }
+
+        var ctx = $('#grafico-torta');
+        var chart = new Chart(ctx, {
+
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: values,
+                    backgroundColor: ["#5603ad", "#9567c6", "#ccb2e8", "#f7efff"],
+
+                }],
+
+                labels: labels
+            }
+        });
+
+        var ctx = $('#grafico-mensile');
+        var chart = new Chart(ctx, {
+
+            type: 'line',
+            data: {
+                labels: arrayMesi,
+                datasets: [{
+                    label: "Fatturato 2017",
+                    borderColor: '#5603ad',
+                    backgroundColor: '#f7efff90',
+                    lineTension: "0",
+                    data: valuesMesi
+                }]
+            },
+        });
+    };
+
+
+}); // fine document ready
--- js/main.js
+++ js/main.js
@@ -1,135 +1,190 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "http://157.230.17.132:4032/sales",
-        method: "GET",
-        success: function(data) {
-            calcValues(data);
 
-        },
-        error: function(err) {
-            alert("Errore AJAX");
-        }
-
-    });
-
-    $("#aggiungi-fatturato").click(function(){
-        var venditore = $(".box-aggiunta-dati .select-venditore").val();
-        var fatturato = parseInt($(".box-aggiunta-dati .input-fatt").val());
-        var periodo = moment($(".box-aggiunta-dati .input-data").val()).format("DD/MM/YYYY");
+    var baseUrl = "http://157.230.17.132:4032/sales";
+    /* Milestone 1:
+    Andamento vendite mensili azienda con grafiico line;
+    fatturato annuo singolo venditore con grafico a torta e percentuale.
+    */
+    stampaGrafici();
+    /* Milestone 2:
+    Crearare possibilità di inserire nuovo fatturato per venditore, importo data,
+    e aggiornare i grafici di conseguenza.
+    */
+    aggiungiFatturato();
 
 
-        $.ajax({
-            url: "http://157.230.17.132:4032/sales",
-            method: "POST",
-            data:{
-                salesman: venditore,
-                amount: fatturato,
-                date: periodo
-            },
-            success: function(data) {
-            },
-            error: function(err){
-                alert("Errore aggiornamento dati")
-            }
-        });
+    // ----------------- FUNZIONI ---------------
 
-        $.ajax({
-            url: "http://157.230.17.132:4032/sales",
+    function stampaGrafici(){
+        $(".charts").empty(); // questo .empty e .append è fdatto per problema di aggioramento grafici di ChartJS
+        $(".charts").append('<canvas id="line-chart"></canvas><canvas id="pie-chart"></canvas>');
+        $.ajax ({
+            url: baseUrl,
             method: "GET",
-            success: function(data) {
-                calcValues(data);
+            success: function (data) {
+                //console.log(data)
+                //Elaboro i dati ricevuti dal server per farne due array
+                //Dò in pasto a ChartJS i due array
+                var datiMensili = costruttoreDatiMensili(data); // elaboriamo i dati di GET per renderli digeribili da ChartJS (ritorna un oggetto)
+                createLineChart(datiMensili.mese, datiMensili.vendita); //Diamo in pasto a ChartJS le labels e data ricavati
+                var fatturato = fatturatoTotale(data);
+                var datiVenditori = costruttoreDatiVenditori(data, fatturato);
+                createPieChart(datiVenditori.venditori, datiVenditori.vendite);
 
             },
-            error: function(err) {
-                alert("Errore AJAX");
+            error: function (err) {
+                alert("Errore API: " + err);
             }
-
         });
+    }
 
-    });
+    function aggiungiFatturato(){
+        $("#btn-invia").click(function (){
+            var nomeVenditore = $("#sel-venditore").val();
+            var dataVendita = moment($("#input-data").val(), "YYYY-MM-DD").format("DD/MM/YYYY");
+            var vendita = $("#input-vendita").val();
 
-
-
-    // --------------- FUNCTIONS ---------------------
+            $.ajax({
+                url: baseUrl,
+                method: "POST",
+                data: {
+                    salesman: nomeVenditore,
+                    amount: vendita,
+                    date: dataVendita
+                },
+                success: function (data){
+                    stampaGrafici();
+                },
+                error: function (err){
+                    alert("Errore aggiornamento dati: " + err);
+                }
+            });
+        });
+    }
 
     function sortNumber(a, b) { // ordine discendente
         return b - a;
     };
 
-    function calcValues(data){
-        var datiOrganizzati = {}; // Torta
-        var datiMensili = {};     // Linea
-        for (var i = 0; i < data.length; i++) {
-            var dato = data[i];
-            var venditore = dato.salesman;
-            var id = dato.id;
-            var calendarioFornito = dato.date;
-            var quantitativo = parseInt(dato.amount);
-            var calendario = moment(calendarioFornito, "DD-MM-YYY");
-            var mese = calendario.format("M");
-            var arrayMesi = moment.months("MMMM");
-
-
-            if (datiOrganizzati[venditore] === undefined) { //Torta
-                datiOrganizzati[venditore] = 0;
-            }
-            datiOrganizzati[venditore] += quantitativo;
-
-            if (datiMensili[mese] === undefined) { //Linea
-                datiMensili[mese] = 0;
-            }
-            datiMensili[mese] += quantitativo;
-
+    function costruttoreDatiMensili(vendite) {
+        var venditeMensili = {
+            gennaio: 0,
+            febbraio: 0,
+            marzo: 0,
+            aprile: 0,
+            maggio: 0,
+            giugno: 0,
+            luglio: 0,
+            agosto: 0,
+            settembre: 0,
+            ottobre: 0,
+            novembre: 0,
+            dicembre: 0,
+        };
+        // var arrayMonths = moment.months(); //---> array dei mesi ottenuto con MomentJS
+        for (var i = 0; i < vendite.length; i++) { // Ciclo nelle vendite ricevute dal GET per aggiungere .amount all'oggetto venditeMensili
+            var vendita = vendite[i]; //valuto ogni singola vendita
+            var dataVendita  = vendita.date; // estrapolo data da oggetto vendita come "DD/MM/YYYY"
+            var meseVendita = moment(dataVendita, "DD/MM/YYYY").format("MMMM"); //trasformo la data nel relativo nome mese, es. "gennaio"
+            venditeMensili[meseVendita] += parseInt(vendita.amount); //uso il nome del mese per riconoscere la chiave nell'oggetto venditeMensili e aggiungere l'amount a quel mese
 
         }
-
-        var labels = [];// Torta
-        var values = [];
-        for (var key in datiOrganizzati) {
-            labels.push(key);
-            values.push(datiOrganizzati[key]);
+        var arrayMesi = []; //inizializzo i due array da usare in ChartJS
+        var arrayVendite = [];
+        for (var nomeMese in venditeMensili) { //Ciclo in oggetto venditeMensili per inserire la coppia key-value in due array, uno con chiave e uno con valore
+            arrayMesi.push(nomeMese); // inserisco il nome del mese nell'array
+            arrayVendite.push(venditeMensili[nomeMese]); // inserisco in arrayVendite la somma delle vendite del mese
         }
-        values.sort(sortNumber);
+        //console.log(arrayMesi, arrayVendite)
+        return {
+            mese: arrayMesi,
+            vendita: arrayVendite
+        };
+    }
 
-
-        var labelsMesi = [];//Linea
-        var valuesMesi = [];
-        for (var key in datiMensili) {
-            labelsMesi.push(key);
-            valuesMesi.push(datiMensili[key]);
+    function fatturatoTotale(vendite){
+        var fatturato = 0;
+        for (var i = 0; i < vendite.length; i++) {
+            var vendita = vendite[i];
+            fatturato += parseInt(vendita.amount);
         }
+        return fatturato;
+    }
 
-        var ctx = $('#grafico-torta');
-        var chart = new Chart(ctx, {
-
-            type: 'pie',
-            data: {
-                datasets: [{
-                    data: values,
-                    backgroundColor: ["#5603ad", "#9567c6", "#ccb2e8", "#f7efff"],
-
-                }],
-
-                labels: labels
+    function costruttoreDatiVenditori(vendite, fatturatoAziendale){
+        var venditeVenditori = {}; //creazione oggetto vuoto che assumerà la somma delle vendite annuali di ogni singolo venditore
+        for (var i = 0; i < vendite.length; i++) { //ciclo for nell'arrey della GET
+            var vendita = vendite[i]; //considero il singolo oggetto dell'array
+            var nomeVenditore = vendita.salesman; //associo a una var il nome del venditore
+            if (venditeVenditori[nomeVenditore] === undefined) { //se non esiste una chiave con quel nome del venditore, la inizializzo con valore 0
+                venditeVenditori[nomeVenditore] = 0;
             }
-        });
+            venditeVenditori[nomeVenditore] += parseInt(vendita.amount); // sommo la vendita dell'oggetto i-esimo a quel venditore
+        }
+        var arrayVenditori = []; //inizializzo i due array da usare in ChartJS
+        var arrayVendite = [];
+        for (var nomeVenditore in venditeVenditori) { //idem come per l'altro grafico
+            arrayVenditori.push(nomeVenditore);
+            var fatturatoPercentualeVenditore = ((venditeVenditori[nomeVenditore] / fatturatoAziendale) * 100).toFixed(2);
+            arrayVendite.push(fatturatoPercentualeVenditore);
+            arrayVendite.sort(sortNumber);
+        }
+        return {
+            venditori: arrayVenditori,
+            vendite: arrayVendite
+        }
+    }
 
-        var ctx = $('#grafico-mensile');
+    function createLineChart(labels, data){
+        var ctx = $('#line-chart');
         var chart = new Chart(ctx, {
 
             type: 'line',
             data: {
-                labels: arrayMesi,
+                labels: labels,
                 datasets: [{
                     label: "Fatturato 2017",
                     borderColor: '#5603ad',
                     backgroundColor: '#f7efff90',
                     lineTension: "0",
-                    data: valuesMesi
+                    data: data
                 }]
             },
         });
-    };
+    }
+
+    function createPieChart(arrayLabels, arrayData){
+        var ctx = $('#pie-chart');
+        var chart = new Chart(ctx, {
+
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: arrayData,
+                    backgroundColor: ["#5603ad", "#9567c6", "#ccb2e8", "#f7efff"],
+
+                }],
+
+                labels: arrayLabels
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }); // fine document ready
